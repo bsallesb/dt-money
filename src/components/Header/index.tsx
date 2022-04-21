705d04dad4b578f6062d14ed406ab7614c519cf4
Breno Oliveira Sales <osalesbreno@gmail.com>
2022-04-20 23:01:26 -0300

    inicial commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import Modal from 'react-modal';
+import logoImg from '../../assets/logo.svg'
+import { Container, Content } from './styles'
+
+interface HeaderProps {
+    OnOpenNewTransactionModal: () => void;
+}
+
+export function Header({ OnOpenNewTransactionModal }: HeaderProps) {
+    return (
+         <Container>
+             <Content>
+             <img src={logoImg} alt="dt money" />
+             <button type='button' onClick={OnOpenNewTransactionModal}>
+                 Nova Transação
+             </button>
+             </Content>
+         </Container>
+    )
+}
